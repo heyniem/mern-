0e5ff055ebb995caef9c2cefe1a0c1b2603e09ba
thanh <trongthanh24062003@gmail.com>
2024-03-14 23:48:27 +0700

    update route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,7 +1,19 @@
 const express = require('express')
-const {createUser,loginUserCtrl} = require('../controller/userCtrl');
+const {
+    createUser,
+    loginUserCtrl,
+    getAllUser,
+    getUser,
+    deleteUser,
+    updateUser
+} = require('../controller/userCtrl');
+const {authMiddleWare} = require('../middlewares/authMiddlewares');
 const router = express.Router();
 
 router.post('/register',createUser);
-router.post('/login',loginUserCtrl)
+router.post('/login',loginUserCtrl);
+router.get('/all_user',getAllUser);
+router.get('/:id',authMiddleWare,getUser)
+router.delete('/:id',deleteUser)
+router.put('/:id',updateUser)
 module.exports = router;
