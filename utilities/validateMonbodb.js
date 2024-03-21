11d1a7c8b7eaef4a1537c5e98f51811f29a3a880
thanh <trongthanh24062003@gmail.com>
2024-03-21 07:44:48 +0700

    add utilities

---
+++ utilities/validateMonbodb.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const validateMongoID = (id) => {
+    const isValid = mongoose.Types.ObjectId.isValid(id);
+    if (!isValid) throw new Error("this is not valid or not found")
+};
+module.exports = {
+    validateMongoID
+}
