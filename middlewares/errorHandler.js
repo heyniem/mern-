d2ebd775ee9d37cb622df1ef6668a8936d7b314e
thanh <trongthanh24062003@gmail.com>
2024-03-21 23:11:03 +0700

    nothing has been done

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,7 +2,7 @@ const notFound = (req,res,next) => {
     const error = new Error(`Not Found: ${req.originUrl}`);
     res.status(404);
     next(error);
-}
+};
 
 const errorHandler = (err, req,res,next) => {
     const statusCode = res.statusCode == 200 ? 500 : res.statusCode;
@@ -11,7 +11,7 @@ const errorHandler = (err, req,res,next) => {
         message: err?.message,
         stack: err?.stack,
     });
-}
+};
 
 module.exports = {
     errorHandler,
