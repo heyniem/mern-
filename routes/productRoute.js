e1f252f52e430e0e79e93bf85734329cc28ccefa
thanh <trongthanh24062003@gmail.com>
2024-03-26 22:22:31 +0700

    minor changes

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const {createProduct} = require("../controller/productController")
+const {createProduct,
+    getSingleProduct,
+    getAllProduct,
+    updateProduct,
+    deleteProduct
+} = require("../controller/productController")
+const {
+    adminCheck,
+    authMiddleWare
+} = require('../middlewares/authMiddlewares');
+router.post('/', authMiddleWare,adminCheck,createProduct);
 
-router.post('/', createProduct);
+router.get('/:id',getSingleProduct);
+router.put('/:id',authMiddleWare,adminCheck,updateProduct)
+router.delete('/:id',authMiddleWare,adminCheck,deleteProduct)
+router.get('/',getAllProduct);
+
+
+
 
 module.exports = router;
